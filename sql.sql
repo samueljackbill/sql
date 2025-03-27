05bb572aa23f28f785caa23aea7c8ab767bed661
Samuel Pires <samueljackbill@gmail.com>
2025-03-26 21:28:59 -0300

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,5 +1,5 @@
 /* CRIAR BANCO DE DADOS */
-CREATE DATABASE brasildigital;
+/* CREATE DATABASE brasildigital;*/
 
 /* CRIAR TABELAS */
 /* curso */
@@ -13,12 +13,8 @@ CREATE TABLE `curso` (
 CREATE TABLE `pessoa` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `idade` int(2) NOT NULL,
-  `cpf` bigint(11) NOT NULL,
-  `rg` text NOT NULL,
-  `endereco` varchar(50) NOT NULL,
-  `telefone` bigint(13) NOT NULL,
-  `email` text NOT NULL
+  `email` varchar(50) NOT NULL,
+  `telefone` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /* matricula */
@@ -30,18 +26,18 @@ CREATE TABLE `matricula` (
 
 /* INSERIR DADOS NA TABELA */
 /* curso */
-INSERT INTO `curso` (`id`, `nome`, `descricao`) VALUES
-(1, 'Lógica de Programação', 'Conhecimentos para Desenvolvimento Web.'),
-(2, 'Marketing Digital', 'Conhecimentos para criar anúncios em Redes Sociais.'),
-(3, 'Rede de Computadores', 'Conhecimentos de topologias e organização de redes.');
+INSERT INTO `curso` (`id`,`nome`, `descricao`) VALUES
+(1,'Lógica de Programação', 'Conhecimentos para Desenvolvimento Web.'),
+(2,'Marketing Digital', 'Conhecimentos para criar anúncios em Redes Sociais.'),
+(3,'Rede de Computadores', 'Conhecimentos de topologias e organização de redes.');
 
 /* pessoa */
-INSERT INTO `pessoa` (`id`, `nome`, `idade`, `cpf`, `rg`, `endereco`, `telefone`, `email`) VALUES
-(1, 'João da Silva', 18, 12345678912, '123.456', 'Rua 1', 5563912345678, 'teste@teste.com'),
-(2, 'Pedro da Silva', 20, 12345678912, '123.456', 'Rua 1', 5563912345678, 'teste@teste.com'),
-(3, 'Maria da Silva', 22, 12345678912, '123.456', 'Rua 1', 5563912345678, 'teste@teste.com'),
-(4, 'Josefa da Silva', 25, 12345678912, '123.456', 'Rua 1', 5563912345678, 'teste@teste.com'),
-(5, 'Neide da Silva', 30, 12345678912, '123.456', 'Rua 1', 5563912345678, 'teste@teste.com');
+INSERT INTO `pessoa` (`id`, `nome`, `telefone`, `email`) VALUES
+(1, 'João da Silva', '5563912345678', 'teste@teste.com'),
+(2, 'Pedro da Silva', '5563912345678', 'teste@teste.com'),
+(3, 'Jorge da Silva', '5563912345678', 'teste@teste.com'),
+(4, 'Lucas da Silva', '5563912345678', 'teste@teste.com'),
+(5, 'Maria da Silva', '5563912345678', 'teste@teste.com');
 
 /* matricula */
 INSERT INTO `matricula` (`id`, `idpessoa`, `idcurso`) VALUES
@@ -51,7 +47,7 @@ INSERT INTO `matricula` (`id`, `idpessoa`, `idcurso`) VALUES
 (4, 2, 3),
 (5, 5, 3);
 
----
+/* --- */
 
 /* SQL que retorna dados de duas tabelas com inner join */
 SELECT 
@@ -69,15 +65,25 @@ FROM
     	curso.id = matricula.idcurso
     );
 
-/* SQL - INSERIR */
+/* SQL - C - CREATE - INSERIR */
 /* 
-    Sintaxe:
-    INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);
+Sintaxe:
+INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);
+
+INSERT INTO pessoa (nome, telefone, email) VALUES ('TESTE', '63984121257', 'teste@teste.com');
+INSERT INTO curso (nome, descricao) VALUES ('Criação de Sites', 'Tudo sobre HTML, CSS e JavaScript');
+*/ 
+
+/* SQL - R - READ - VISUALIZAR */
+/*
+Sintaxe: 
+SELECT column1, column2, ...
+FROM table_name;
+
+SELECT * FROM pessoa;
+SELECT * FROM pessoa WHERE id = 4;
 */
-INSERT INTO curso (id, nome, descricao) VALUES (4, 'Criação de Sites', 'Tudo sobre HTML, CSS e JavaScript');
 
-/* SQL - ATUALIZAR */
+/* SQL - U - UPDATE - ATUALIZAR */
 
-/* SQL - APAGAR */
-
-/* SQL - VISUALIZAR */
+/* SQL - D - DELETE - APAGAR */
